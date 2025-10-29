d0739076a591ea766b5fd759a73c21506e8ab349
Hakase <hakase@litdevs.org>
2025-10-29 23:19:09 +0100

    Add other half of goose to templates

--- tools/seal.py
+++ tools/seal.py
@@ -4,11 +4,12 @@ from aformatter import per_line_entries
 from oneoff.calculate_center import polylabel
 from oneoff.calculate_polygon import pongon
 
-# constants
+# edit these for your canvas (if someone is actually reusing this code lol)
 top_left_tile = [1084,594]
 bottom_right_tile = [1086,595]
 canvas_offset = [0,0]
 canvas_crop = [926,33,1495,1729] # the first two (left,top) affect template locations, the last two (right,bottom) are actual crops
+# editables end here
 
 template_regex = re.compile(r"top\/(\d+) (\d+)\/(\d+) (\d+) (.+)")
 canvas = Image.new("RGBA", [
@@ -16,7 +17,6 @@ canvas = Image.new("RGBA", [
 	(abs(bottom_right_tile[1] - top_left_tile[1])+1)*1000
 ], (0,0,0,0))
 
-# load atlas items (don't want to create dupes)
 new_atlas_items = []
 with open('web/atlas.json', 'r', encoding='utf-8') as atlas_file:
 	atlas_data = json.loads(atlas_file.read())
@@ -40,20 +40,26 @@ for file in seal_zip.namelist():
 	tname = template_data.group(5)
 
 	if not (tlx >= top_left_tile[0] and tlx <= bottom_right_tile[0]) or not (tly >= top_left_tile[1] and tly <= bottom_right_tile[1]):
+		# temp is outside the tile area, so quick skipping
 		continue
 
 	# atlas data
 	x = tx + ((tlx-top_left_tile[0])*1000) - canvas_crop[0]
 	y = ty + ((tly-top_left_tile[1])*1000) - canvas_crop[1]
 	name = os.path.splitext(tname)[0]
-	id = f"{name.replace(" ","")}_{x}_{y}"
+	sid = f"{name.replace(" ", "")}_{x}_{y}"
 	seal = "https://seal.hakase.life/" + urllib.parse.quote(f"{tlx} {tly}/{tx} {ty} {tname}")
 
-	if x < 0 or y < 0:
+	if (x < 0 or y < 0) or (x>=(canvas_crop[2]+canvas_offset[0]) or y>=(canvas_crop[3]+canvas_offset[1])):
+		# temp starts outside the crop, so quick skipping
 		continue
 
 	pillow_template = Image.open(seal_zip.open(file)).convert("RGBA")
 	canvas.paste(pillow_template, (x, y), pillow_template)
+
+	if (pillow_template.width+x)>(canvas_crop[2]+canvas_offset[0]) or (pillow_template.height+y)>(canvas_crop[3]+canvas_offset[1]):
+		# temp ends outside the crop, so reminding me to update the var
+		print("Entry", sid, "is going off the canvas! Update canvas_crop.")
 
 	if seal in existing_atlas_seals or name == "roads" or name.endswith(" bg"):
 		continue
@@ -61,7 +67,7 @@ for file in seal_zip.namelist():
 	polygon = pongon(pillow_template, x+canvas_offset[0], y+canvas_offset[1])
 
 	new_atlas_items.append({
-		"id": id,
+		"id": sid,
 		"name": name,
 		"description": "FIXME",
 		"writers": ["bot:SEAL"],
